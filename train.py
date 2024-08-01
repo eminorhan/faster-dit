42fbc70232e946f9e1ec47d5753d6aa78f9840e6
eminorhan <aeminorhan@gmail.com>
2024-08-01 15:25:09 -0400

    update train: remove accelerate, whole data loaded to cpu RAM, currently assume 1 GPU

--- train.py
+++ train.py
@@ -11,7 +11,6 @@ import torch
 # the first flag below was False when we tested this script but True makes A100 training a lot faster:
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
-from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from collections import OrderedDict
 from PIL import Image
@@ -21,7 +20,6 @@ from time import time
 import argparse
 import logging
 import os
-from accelerate import Accelerator
 
 from models import DiT_models
 from diffusion import create_diffusion
@@ -83,26 +81,6 @@ def center_crop_arr(pil_image, image_size):
     return Image.fromarray(arr[crop_y: crop_y + image_size, crop_x: crop_x + image_size])
 
 
-class CustomDataset(Dataset):
-    def __init__(self, features_dir, labels_dir):
-        self.features_dir = features_dir
-        self.labels_dir = labels_dir
-
-        self.features_files = sorted(os.listdir(features_dir))
-        self.labels_files = sorted(os.listdir(labels_dir))
-
-    def __len__(self):
-        assert len(self.features_files) == len(self.labels_files), "Number of feature files and label files should be same"
-        return len(self.features_files)
-
-    def __getitem__(self, idx):
-        feature_file = self.features_files[idx]
-        label_file = self.labels_files[idx]
-
-        features = np.load(os.path.join(self.features_dir, feature_file))
-        labels = np.load(os.path.join(self.labels_dir, label_file))
-        return torch.from_numpy(features), torch.from_numpy(labels)
-
 #################################################################################
 #                                  Training Loop                                #
 #################################################################################
@@ -113,20 +91,18 @@ def main(args):
     """
     assert torch.cuda.is_available(), "Training currently requires at least one GPU."
 
-    # Setup accelerator:
-    accelerator = Accelerator()
-    device = accelerator.device
+    # Setup device:
+    device = torch.device(args.device)
 
     # Setup an experiment folder:
-    if accelerator.is_main_process:
-        os.makedirs(args.results_dir, exist_ok=True)  # Make results folder (holds all experiment subfolders)
-        experiment_index = len(glob(f"{args.results_dir}/*"))
-        model_string_name = args.model.replace("/", "-")  # e.g., DiT-XL/2 --> DiT-XL-2 (for naming folders)
-        experiment_dir = f"{args.results_dir}/{experiment_index:03d}-{model_string_name}"  # Create an experiment folder
-        checkpoint_dir = f"{experiment_dir}/checkpoints"  # Stores saved model checkpoints
-        os.makedirs(checkpoint_dir, exist_ok=True)
-        logger = create_logger(experiment_dir)
-        logger.info(f"Experiment directory created at {experiment_dir}")
+    os.makedirs(args.results_dir, exist_ok=True)  # Make results folder (holds all experiment subfolders)
+    experiment_index = len(glob(f"{args.results_dir}/*"))
+    model_string_name = args.model.replace("/", "-")  # e.g., DiT-XL/2 --> DiT-XL-2 (for naming folders)
+    experiment_dir = f"{args.results_dir}/{experiment_index:03d}-{model_string_name}"  # Create an experiment folder
+    checkpoint_dir = f"{experiment_dir}/checkpoints"  # Stores saved model checkpoints
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    logger = create_logger(experiment_dir)
+    logger.info(f"Experiment directory created at {experiment_dir}")
 
     # Create model:
     assert args.image_size % 8 == 0, "Image size must be divisible by 8 (for the VAE encoder)."
@@ -138,32 +114,20 @@ def main(args):
     ema = deepcopy(model).to(device)  # Create an EMA of the model for use after training
     requires_grad(ema, False)
     diffusion = create_diffusion(timestep_respacing="")  # default: 1000 steps, linear noise schedule
-    if accelerator.is_main_process:
-        logger.info(f"DiT Parameters: {sum(p.numel() for p in model.parameters()):,}")
+    logger.info(f"DiT Parameters: {sum(p.numel() for p in model.parameters()):,}")
 
     # Setup optimizer (we used default Adam betas=(0.9, 0.999) and a constant learning rate of 1e-4 in our paper):
     opt = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0)
 
     # Setup data:
-    features_dir = f"{args.feature_path}/imagenet256_features"
-    labels_dir = f"{args.feature_path}/imagenet256_labels"
-    dataset = CustomDataset(features_dir, labels_dir)
-    loader = DataLoader(
-        dataset,
-        batch_size=int(args.global_batch_size // accelerator.num_processes),
-        shuffle=True,
-        num_workers=args.num_workers,
-        pin_memory=True,
-        drop_last=True
-    )
-    if accelerator.is_main_process:
-        logger.info(f"Dataset contains {len(dataset):,} images ({args.feature_path})")
+    train_data = torch.load(args.train_data_path, map_location='cpu')
+    n_train = train_data['targets'].shape[0]
+    logger.info(f"Dataset contains {n_train:,} images ({args.train_data_path})")
 
     # Prepare models for training:
     update_ema(ema, model, decay=0)  # Ensure EMA is initialized with synced weights
     model.train()  # important! This enables embedding dropout for classifier-free guidance
     ema.eval()  # EMA model should always be in eval mode
-    model, opt, loader = accelerator.prepare(model, opt, loader)
 
     # Variables for monitoring/logging purposes:
     train_steps = 0
@@ -171,68 +135,69 @@ def main(args):
     running_loss = 0
     start_time = time()
     
-    if accelerator.is_main_process:
-        logger.info(f"Training for {args.epochs} epochs...")
-    for epoch in range(args.epochs):
-        if accelerator.is_main_process:
-            logger.info(f"Beginning epoch {epoch}...")
-        for x, y in loader:
-            x = x.to(device)
-            y = y.to(device)
-            x = x.squeeze(dim=1)
-            y = y.squeeze(dim=1)
-            t = torch.randint(0, diffusion.num_timesteps, (x.shape[0],), device=device)
-            model_kwargs = dict(y=y)
-            loss_dict = diffusion.training_losses(model, x, t, model_kwargs)
-            loss = loss_dict["loss"].mean()
-            opt.zero_grad()
-            accelerator.backward(loss)
-            opt.step()
-            update_ema(ema, model)
+    # infinite stream of training data
+    while True:
+        # randomly sample indices
+        indx = torch.randint(0, n_train, size=(args.batch_size, ))
 
-            # Log loss values:
-            running_loss += loss.item()
-            log_steps += 1
-            train_steps += 1
-            if train_steps % args.log_every == 0:
-                # Measure training speed:
-                torch.cuda.synchronize()
-                end_time = time()
-                steps_per_sec = log_steps / (end_time - start_time)
-                # Reduce loss history over all processes:
-                avg_loss = torch.tensor(running_loss / log_steps, device=device)
-                avg_loss = avg_loss.item() / accelerator.num_processes
-                if accelerator.is_main_process:
-                    logger.info(f"(step={train_steps:07d}) Train Loss: {avg_loss:.4f}, Train Steps/Sec: {steps_per_sec:.2f}")
-                # Reset monitoring variables:
-                running_loss = 0
-                log_steps = 0
-                start_time = time()
+        # take the corresponding slices of data
+        samples = train_data['latents'][indx]
+        targets = train_data['targets'][indx]
 
-            # Save DiT checkpoint:
-            if train_steps % args.ckpt_every == 0 and train_steps > 0:
-                if accelerator.is_main_process:
-                    checkpoint = {
-                        "model": model.module.state_dict(),
-                        "ema": ema.state_dict(),
-                        "opt": opt.state_dict(),
-                        "args": args
-                    }
-                    checkpoint_path = f"{checkpoint_dir}/{train_steps:07d}.pt"
-                    torch.save(checkpoint, checkpoint_path)
-                    logger.info(f"Saved checkpoint to {checkpoint_path}")
+        # move to gpu
+        samples = samples.to(device, non_blocking=True)
+        targets = targets.to(device, non_blocking=True)
 
-    model.eval()  # important! This disables randomized embedding dropout
-    # do any sampling/FID calculation/etc. with ema (or model) in eval mode ...
-    
-    if accelerator.is_main_process:
-        logger.info("Done!")
+        # not sure if I really need the squeeze() here
+        samples = samples.squeeze(dim=1)
+        targets = targets.squeeze(dim=1)
+
+        t = torch.randint(0, diffusion.num_timesteps, (samples.shape[0],), device=device)
+        model_kwargs = dict(y=targets)
+        loss_dict = diffusion.training_losses(model, samples, t, model_kwargs)
+        loss = loss_dict["loss"].mean()
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+        update_ema(ema, model)
+
+        # Log loss values:
+        running_loss += loss.item()
+        log_steps += 1
+        train_steps += 1
+        if train_steps % args.log_every == 0:
+            # Measure training speed:
+            torch.cuda.synchronize()
+            end_time = time()
+            steps_per_sec = log_steps / (end_time - start_time)
+            # Reduce loss history over all processes:
+            avg_loss = torch.tensor(running_loss / log_steps, device=device)
+            avg_loss = avg_loss.item()
+            logger.info(f"(step={train_steps:07d}) Train Loss: {avg_loss:.4f}, Train Steps/Sec: {steps_per_sec:.2f}")
+            # Reset monitoring variables:
+            running_loss = 0
+            log_steps = 0
+            start_time = time()
+
+        # Save DiT checkpoint:
+        if train_steps % args.ckpt_every == 0 and train_steps > 0:
+            checkpoint = {
+                "model": model.module.state_dict(),
+                "ema": ema.state_dict(),
+                "opt": opt.state_dict(),
+                "args": args
+            }
+            checkpoint_path = f"{checkpoint_dir}/{train_steps:07d}.pt"
+            torch.save(checkpoint, checkpoint_path)
+            logger.info(f"Saved checkpoint to {checkpoint_path}")
+
+    # TODO: optionally do sampling/FID calculation etc. with ema (or model) in eval mode
 
 
 if __name__ == "__main__":
     # Default args here will train DiT-XL/2 with the hyperparameters we used in our paper (except training iters).
     parser = argparse.ArgumentParser()
-    parser.add_argument("--feature-path", type=str, default="features")
+    parser.add_argument('--train_data_path', default='', type=str)
     parser.add_argument("--results-dir", type=str, default="results")
     parser.add_argument("--model", type=str, choices=list(DiT_models.keys()), default="DiT-XL/2")
     parser.add_argument("--image-size", type=int, choices=[256, 512], default=256)
@@ -241,7 +206,6 @@ if __name__ == "__main__":
     parser.add_argument("--global-batch-size", type=int, default=256)
     parser.add_argument("--global-seed", type=int, default=0)
     parser.add_argument("--vae", type=str, choices=["ema", "mse"], default="ema")  # Choice doesn't affect training
-    parser.add_argument("--num-workers", type=int, default=4)
     parser.add_argument("--log-every", type=int, default=100)
     parser.add_argument("--ckpt-every", type=int, default=50_000)
     args = parser.parse_args()
